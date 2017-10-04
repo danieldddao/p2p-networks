c00c62201f8a3b6c8d1654fa51a7ef580ccde865
Daniel Dao <danieldao@Daniels-MacBook-Pro.local>
2017-10-04 09:43:02 -0500

    added book table

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,11 @@
 class BooksController < ApplicationController
+  require 'socket'
 
   def index
-
+    puts "index page:"
+    public_id = request.remote_ip
+    local_ip = Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
+    puts local_ip.ip_address
   end
 
   def search
