d21d3fd877a2d7192f1e086cab7a95148ceb297a
Daniel Dao <danieldao@Daniels-MacBook-Pro.local>
2017-10-04 17:57:49 -0500

    added page for sharing new book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,35 @@
 class BooksController < ApplicationController
   require 'socket'
 
+  def book_params
+    params.require(:book).permit(:isbn, :title, :author, :location)
+  end
+
   def index
     puts "index page:"
     public_id = request.remote_ip
     local_ip = Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
+    puts public_id
     puts local_ip.ip_address
+  end
+
+  def new
+    # render new page
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    puts @book.title
+    puts @book.isbn
+    puts @book.author
+    puts @book.location
+      # if @book.save
+    #   flash[:notice] = "#{@book.title} was successfully added."
+    #   redirect_to root_path
+    # else
+    #   render 'new'
+    # end
   end
 
   def search
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'books/index'
+  get 'books/new'
+  post 'books/new', to: 'books#create'
   root 'books#index'
   post 'books/search', to: 'books#search'
 end
